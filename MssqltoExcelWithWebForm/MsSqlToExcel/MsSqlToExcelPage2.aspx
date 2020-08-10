d8fdd44402ccd1a194d5265de50d41b93eb21b2d
UtkuDemirbas <48966595+UtkuDemirbas@users.noreply.github.com>
2020-08-10 11:27:05 +0300

    Proje and db added.

---
+++ MssqltoExcelWithWebForm/MsSqlToExcel/MsSqlToExcelPage2.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MsSqlToExcelPage2.aspx.cs" Inherits="MsSqlToExcel.MsSqlToExcelPage2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>MssqlToExcel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+</head>
+<body >
+    
+        <form runat="server" >
+            <div class="d-flex justify-content-center" >
+                <asp:GridView ID="GridExcel" runat="server" AutoGenerateColumns="true" ShowHeaderWhenEmpty="true" CssClass="table  table-responsive table-striped ml-2">
+                    <Columns>
+                    </Columns>
+                </asp:GridView>
+            </div>
+            <div class="row">
+                <asp:Button runat="server" ID="BtnDataYükle" CssClass="btn btn-danger ml-3" Text="Sayfaya Yükle" OnClick="BtnDataYükle_Click" />
+                <asp:Button ID="BtnKaydet" runat="server" CssClass="btn btn-danger ml-2" Text="Kaydet" OnClick="BtnKaydet_Click" />
+            </div>
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </form>
+   
+</body>
+</html>
+
---
+++ MssqltoExcelWithWebForm/MsSqlToExcel/MsSqlToExcelPage2.aspx.cs
@@ -0,0 +1,73 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace MsSqlToExcel
+{
+	public partial class MsSqlToExcelPage2 : System.Web.UI.Page
+	{
+		string conn = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;//db ConnectionString
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+		string Query = string.Format(@"SELECT [hgsID],[hgsNumarasi],[aciklama] FROM [dbo].[Hgs_Kurum]");
+		protected void BtnKaydet_Click(object sender, EventArgs e)
+		{
+			
+			using (SqlConnection con = new SqlConnection(conn))
+			{
+				using (SqlCommand cmd = new SqlCommand(Query))
+				{
+					using (SqlDataAdapter sda = new SqlDataAdapter())
+					{
+						cmd.Connection = con;
+						sda.SelectCommand = cmd;
+						using (DataTable dt = new DataTable())
+						{
+							sda.Fill(dt);
+							using (XLWorkbook wb = new XLWorkbook())
+							{
+								wb.Worksheets.Add(dt, "DEMO");
+								Response.Clear();
+								Response.Buffer = true;
+								Response.Charset = "";
+								Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+								Response.AddHeader("content-disposition", "attachment;filename=DEMO.xlsx");
+								using (MemoryStream MyMemoryStream = new MemoryStream())
+								{
+									wb.SaveAs(MyMemoryStream);
+									MyMemoryStream.WriteTo(Response.OutputStream);
+									Response.Flush();
+									Response.End();
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		protected void BtnDataYükle_Click(object sender, EventArgs e)
+		{
+			
+			using (SqlConnection con = new SqlConnection(conn))
+			{
+				using (SqlCommand cmd = new SqlCommand(Query, con))
+				{
+					using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+					{
+						DataTable dt = new DataTable();
+						sda.Fill(dt);
+						GridExcel.DataSource = dt;
+						GridExcel.DataBind();
+					}
+				}
+			}
+		}
+	}
+}
